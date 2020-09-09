d1b54f55ca1d47aaeab2d8918dd7346287e3d6d9
loochek <m.ziganshin.zm@gmail.com>
2020-09-10 00:23:59 +0300

    Under construction!

--- main.c
+++ main.c
@@ -3,27 +3,112 @@
 #include <stdlib.h>
 #include <assert.h>
 
-/* ввод вывод обернуть в функции
+/* ввод вывод обернуть в функции done
 свой strcmp
-malloc
+malloc done
 */
 
-const int MAX_STRING = 100;
+#define MAX_LENGTH 100
+#define MAX_STRINGS_COUNT 10000
 
-// чтобы не использовать malloc лишний раз, выделим память заранее в сегменте данных
-// (ИМХО сложно его использовать, не зная размер входных данных)
-// (так что, надеюсь, за такое выделение меня не побьют        )
-// 10000 строк по 100 символов хватит
-char mem[10000][MAX_STRING];
-// еще будем хранить указатели на строки - их и будем сортировать
-char* strings[10000];
+// Будем хранить указатели на строки - их и будем сортировать
+// Сами строки будем хранить в памяти, выделяемой malloc'ом
+char* strings[MAX_STRINGS_COUNT];
+
+// так как библиотечная не может в русские буквы
+int is_alpha(unsigned char c)
+{
+    return ((c >= 'a' && c <= 'z') ||
+        (c >= 'A' && c <= 'Z') ||
+        (c >= 192 && c <= 223) ||
+        (c >= 224 && c <= 255));
+}
 
 // компаратор строк. Обратите внимание, что раз сортить мы будем указатели,
 // то qsort будет нам передавать указатели на указатели на строки
-int comp(const void *a, const void *b)
+int comp(const void *f, const void *s)
 {
-    // разыменовываем УнУнС просто в указатель на строку
-    return strcmp(*(const char**)a, *(const char**)b);
+    // разыменовываем УнУнС просто в указатели на строку
+    const unsigned char *a = *(const char**)f;
+    const unsigned char *b = *(const char**)s;
+
+    // сравниваем строки a и b без учета знаков препинания и пробелов
+    // иначе говоря, рассмариваем только буквы a..z A..Z а..я А...Я
+
+    while (*a != '\0' && *b != '\0')
+    {
+        // игнорируем не-буквы
+        while (!is_alpha(*a) && *a != '\0')
+            a++;
+        while (!is_alpha(*b) && *b != '\0')
+            b++;
+        if (*a == 0 || *b == 0)
+            break;
+
+        printf("%c %c\n", *a, *b);
+        printf("%u %u\n", *a, *b);
+        if (*a > *b)
+            return 1;
+        else if (*a < *b)
+            return -1;
+        else
+        {
+            a++;
+            b++;
+        }
+        
+    }
+    if (*a != '\0' && *b == '\0')
+        return 1;
+    else if (*a == '\0' && *b != '\0')
+        return -1;
+    else
+        return 0;
+}
+
+// считывает строки из файла
+// результат: str_cnt указателей на строки в strings
+// не забудьте освободить эту память!
+int read_input()
+{
+    FILE *fd = fopen("onegin.txt", "r");
+    // счетчик строк
+    int str_cnt = 0;
+    // выделяем память под первую строку
+    char *str_ptr = malloc(sizeof(char) * MAX_LENGTH);
+    // считываем строки и записываем указатели на них в strings
+    while (str_cnt < MAX_STRINGS_COUNT && fgets(str_ptr, MAX_LENGTH, fd) != NULL)
+    {
+        strings[str_cnt] = str_ptr;
+        // выделяем память под очередную строку
+        str_ptr = malloc(sizeof(char) * MAX_LENGTH);
+        str_cnt++;
+    }
+    // освобождаем последнюю неиспользованную память
+    // (издержки реализации)
+    // (зато ничего не копируем)
+    free(str_ptr);
+    fclose(fd);
+
+    // возвращаем количество считанных строк
+    return str_cnt;
+}
+
+// пишет строки из strings в файл
+void write_output(int str_cnt)
+{
+    // выводим первые str_cnt строк из strings
+    FILE *fdout = fopen("onegin_parsed.txt", "w");
+    for (int i = 0; i < str_cnt; i++)
+        fputs(strings[i], fdout);
+    fclose(fdout);
+}
+
+// освобождает память
+void cleanup(int str_cnt)
+{
+    for (int i = 0; i < str_cnt; i++)
+        free(strings[i]);
 }
 
 void unit_test_1()
@@ -62,23 +147,13 @@ int main()
 #ifdef DO_UNIT_TESTS
     do_unit_tests();
 #endif
-    FILE *fd = fopen("onegin.txt", "r");
-    // счетчик строк
-    int str_cnt = 0;
-    // считываем строки пока они есть и записываем указатели на них в strings
-    while (fgets(mem[str_cnt], MAX_STRING, fd) != NULL)
-    {
-        strings[str_cnt] = mem[str_cnt];
-        str_cnt++;
-    }
-    fclose(fd);
+    // читаем файл5
+    int str_cnt = read_input();
     // еще раз: сортим указатели!
     qsort(strings, str_cnt, sizeof(char*), comp);
-
-    // выводим в файл
-    FILE *fdout = fopen("onegin_parsed.txt", "w");
-    for (int i = 0; i < str_cnt; i++)
-        fputs(strings[i], fdout);
-    fclose(fdout);
+    // выводим
+    write_output(str_cnt);
+    // освобождаем память
+    cleanup(str_cnt);
     return 0;
 }
