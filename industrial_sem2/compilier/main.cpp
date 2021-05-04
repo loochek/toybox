f8f17695d151d7f1d2c06a2b4655cd98042205ed
loochek <m.ziganshin.zm@gmail.com>
2021-05-05 01:02:58 +0300

    Version number update

--- industrial_sem2/compilier/main.cpp
+++ industrial_sem2/compilier/main.cpp
@@ -113,7 +113,7 @@ int main(int argc, char *argv[])
             show_ast = true;
         else if (strcmp(argv[arg_cnt], "--about") == 0)
         {
-            printf("Tiny Compilier ver 0.95\n"
+            printf("Tiny Compilier 0.98\n"
                    "Copyleft loochek 2021\n");
             return 0;
         }
