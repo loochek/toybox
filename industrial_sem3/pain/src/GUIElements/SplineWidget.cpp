fbe4c54062148d195114a8800e7808862c7e200f
loochek <m.ziganshin.zm@gmail.com>
2021-11-08 11:37:14 +0300

    Spline optimization

--- industrial_sem3/pain/src/GUIElements/SplineWidget.cpp
+++ industrial_sem3/pain/src/GUIElements/SplineWidget.cpp
@@ -3,7 +3,7 @@
 #include "SplineWidget.hpp"
 
 const float SPLINE_THICKNESS = 2.0f;
-const float SPLINE_STEP      = 0.5f;
+const float SPLINE_STEP      = 10.0f;
 
 SplineWidget::SplineWidget(const IntRect &widgetRect, Widget *parent) :
     Widget(widgetRect, parent)
@@ -29,6 +29,20 @@ void SplineWidget::onRedrawThis()
         
     float maxParam = spline.getMaxParam();
 
+    Vec2f prevPoint = spline[0];
+
     for (float t = 0; t <= maxParam; t += SPLINE_STEP)
-        mTexture.drawCircle(spline[t], SPLINE_THICKNESS, LGL::Color::Black);
+    {
+        Vec2f currPoint = spline[t];
+        mTexture.drawCircle(currPoint, SPLINE_THICKNESS, LGL::Color::Black);
+        mTexture.drawLine(prevPoint, currPoint, SPLINE_THICKNESS, LGL::Color::Black);
+        prevPoint = currPoint;
+    }
+
+    // to avoid flickering with big step
+
+    Vec2f currPoint = spline[maxParam];
+
+    mTexture.drawCircle(currPoint, SPLINE_THICKNESS, LGL::Color::Black);
+    mTexture.drawLine(prevPoint, currPoint, SPLINE_THICKNESS, LGL::Color::Black);
 }
